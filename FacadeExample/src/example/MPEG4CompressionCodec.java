718db2c837c36758f700a02fd83b0abe9397c8a8
Lauro Aranha <84295347+LauroAranha@users.noreply.github.com>
2023-09-11 21:19:03 -0300

    Add files via upload

---
+++ FacadeExample/src/example/MPEG4CompressionCodec.java
@@ -0,0 +1,5 @@
+package example;
+
+public class MPEG4CompressionCodec implements Codec {
+    public String type = "mp4";
+}
---
+++ FacadeExample/src/example/OggCompressionCodec.java
@@ -0,0 +1,5 @@
+package example;
+
+public class OggCompressionCodec implements Codec {
+    public String type = "ogg";
+}
